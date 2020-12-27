1e965bbd7f381360971f5e39e8878f5a18c51f47
Lakin Wecker <lakin@structuredabstraction.com>
2020-12-27 08:47:23 -0700

    Adding AGPL license. Forgot it for the first commit. :Doh!:

--- src/chessio.rs
+++ src/chessio.rs
@@ -1,3 +1,20 @@
+// Copyright 2020 Lakin Wecker
+//
+// This file is part of lila-deepq.
+//
+// lila-deepq is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// lila-deepq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with lila-deepq.  If not, see <https://www.gnu.org/licenses/>.
+
 //--------------------------------------------------------------------------
 // Implement the serde serialization/deserialization traits for Fen.
 // We can't implement the trait for a struct from a different crate and for
--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,20 @@
+// Copyright 2020 Lakin Wecker
+//
+// This file is part of lila-deepq.
+//
+// lila-deepq is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// lila-deepq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with lila-deepq.  If not, see <https://www.gnu.org/licenses/>.
+
 use mongodb::error::{Error as _MongoDBError};
 use mongodb::bson::de::{Error as _BsonError};
 //use serde::de::{Error as _SerdeDeError};
